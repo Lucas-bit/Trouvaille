9c1e9deac57f79026f454265444893812c18788a
Joey Register <josephregister1258@gmail.com>
2019-12-20 18:49:03 -0500

    countries dropdown dynamically fills

--- javascript.js
+++ javascript.js
@@ -1,6 +1,9 @@
-var selectedRegion = $("#continents").val()
 
-$("document").ready(function() {
+
+$("#continents").on("change",function() {
+    $("#countries").empty()
+    var selectedRegion = $("#continents").val()
+    
     $.ajax({
         method: "GET",
         url: "https://restcountries.eu/rest/v2/region/"+selectedRegion 
@@ -9,12 +12,36 @@ $("document").ready(function() {
 //dropdown for countries
                 var countries = []
             for (var i = 0;i<results.length;i++) {
-                countries.push(results[i].name)
-                var country = $("<option>").val(countries[i])
-                $("#countries").append(country)
-                }
+                    countries.push(results[i].name)
+                    var country = $("<option>").text(countries[i])
+                    $("#countries").append(country)
+                    }
             console.log(countries)
-            console.log($(".dropdown").html())
+            console.log($("#countries").html())
+                
+
+
+            })
+})
+
+$("#countries").on("change",function() {
+    $("#cities").empty()
+    var selectedRegion = $("#continents").val()
+    
+    $.ajax({
+        method: "GET",
+        url: "https://restcountries.eu/rest/v2/region/"+selectedRegion 
+    }).then(function(response) {
+                var results = response
+//dropdown for countries
+                var countries = []
+            for (var i = 0;i<results.length;i++) {
+                    countries.push(results[i].name)
+                    var country = $("<option>").text(countries[i])
+                    $("#countries").append(country)
+                    }
+            console.log(countries)
+            console.log($("#countries").html())
                 
 
 
