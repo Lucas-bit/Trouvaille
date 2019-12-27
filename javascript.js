b09691c2a6b6105cb1270122315852a5b322ea72
Lucas Gonzalez <lucasbertaudgonzalez@gmail.com>
2019-12-27 14:57:49 -0500

    working slider, need to adjust the positioning and size

--- javascript.js
+++ javascript.js
@@ -1,3 +1,5 @@
+
+
 $("#continents").on("change", function () {
     $("#countries").empty()
     var selectedRegion = $("#continents").val()
@@ -64,17 +66,50 @@ $("#logo-bottom").on("click", function(){
 
 
 //---------------------------------slide-show------------------------------------------//
-// var slideIndex = 0;
-// showSlides();
 
-// function showSlides() {
-//   var i;
-//   var slides = document.getElementsByClassName("mySlides");
-//   for (i = 0; i < slides.length; i++) {
-//     slides[i].style.display = "none";
-//   }
-//   slideIndex++;
-//   if (slideIndex > slides.length) {slideIndex = 1}
-//   slides[slideIndex-1].style.display = "block";
-// //   setTimeout(showSlides, 2000); // Change image every 2 seconds
-// // }
+const carouselSlide = document.querySelector(".carousel-slide");
+const carouselImages = document.querySelectorAll(".carousel-slide img");
+
+
+//buttons
+const prevBtn = document.querySelector("#prevBtn");
+const nextBtn = document.querySelector("#nextBtn");
+
+//Counter
+let counter = 1; 
+const size = carouselImages[0].clientWidth; // know how much to slide
+
+carouselSlide.style.transform = "translateX(" + (-size * counter) + "px)"; // starts us at the first pic
+
+//Button listeners
+
+nextBtn.addEventListener("click", function () {
+    if (counter >= carouselImages.length -1 ) return; //dont do anything
+    carouselSlide.style.transition = "transform 0.4s ease-in-out"; // creates transition
+    counter ++; //adds to our counter
+    console.log(counter)
+    carouselSlide.style.transform = "translateX(" + (-size * counter) + "px)";
+});
+
+
+prevBtn.addEventListener("click", function () {
+    if (counter <= 0 ) return; //dont do anything
+    carouselSlide.style.transition = "transform 0.4s ease-in-out"; // creates speed of transition
+    counter --; //takes from our counter
+    console.log(counter)
+    carouselSlide.style.transform = "translateX(" + (-size * counter) + "px)";
+});
+
+carouselSlide.addEventListener("transitionend", function(){ //listens to when the slide stops moving
+    console.log(carouselImages[counter]);
+    if (carouselImages[counter].id === "lastClone"){
+        carouselSlide.style.transition = "none";
+        counter = carouselImages.length -2; 
+        carouselSlide.style.transform = "translateX(" + (-size * counter) + "px)";
+    }
+    if (carouselImages[counter].id === "firstClone"){
+        carouselSlide.style.transition = "none";
+        counter = carouselImages.length - counter; 
+        carouselSlide.style.transform = "translateX(" + (-size *counter) + "px)";
+    }
+})
