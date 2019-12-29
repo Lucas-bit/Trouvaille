a2d8055f1acdf5b665844980d51c33340dbd11af
Joey Register <josephregister1258@gmail.com>
2019-12-29 16:19:50 -0500

    ajax for google places

--- javascript.js
+++ javascript.js
@@ -1,3 +1,5 @@
+var cities = []
+
 $("#continents").on("change", function () {
     $("#countries").empty()
     var selectedRegion = $("#continents").val()
@@ -45,10 +47,39 @@ $("#countries").on("change", function () {
                 var results = response
 //dropdown for cities
 console.log(results.data[0].name)
-                var cities = []
+                
             for (var i = 0;i<10;i++) {
-                    cities.push(results.data[i].name)
-                    var city = $("<option>").text(cities[i])
-                    $("#cities").append(city)
+                    cities.push({name:results.data[i].name,lat:results.data[i].latitude,lon:results.data[i].longitude})
                     
-}})})
+                    var city = $("<option>").text(cities[i].name)
+                    $("#cities").append(city)
+
+                    
+                    
+}
+})})
+
+
+$("#cities").on("change", function() {
+    var googleKey = "AIzaSyA2-3Fi1nZ7Ep570B8W28x4lmGxY5UqRlc"
+    var selectedCity = $("#cities").val()
+
+    for(var i=0;i<cities.length;i++) {
+        if (selectedCity===cities[i].name) {
+            var lat = cities[i].lat
+            var lon = cities[i].lon
+        }
+    }
+    $.ajax({
+        method: "GET",
+        crossOrigin: true,
+        url: "https://cors-anywhere.herokuapp.com/https://maps.googleapis.com/maps/api/place/nearbysearch/json?location="+lat+","+lon+"&radius=2000&type=restaurant&key="+googleKey,
+    }).then(function(response) {
+            console.log(response)
+
+           var photoURL = "https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference="+response.results[i].photos[0].photo_reference+"&key="+googleKey
+        }
+    
+    
+    )})
+
